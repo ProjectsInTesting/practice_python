a1e292f7623f16cbabe9653e8506f33e781afd75
herrafiisdet <herrafiisdet@gmail.com>
2025-02-05 17:33:01 -0500

    i 02/05/25 desktop

--- if_statement/if_practice_1.py
+++ if_statement/if_practice_1.py
@@ -1,2 +1,10 @@
-username = "admin"
-print (username)
+db_username = "admin"
+db_password = "123456"
+
+username = input ("Please enter your username:")
+password = input ("please enter your password: ")
+
+if username == db_username and password == db_password:
+    print ("You logged in Successfully!")
+else:
+    print ("wrong credentials used! please try agian")
