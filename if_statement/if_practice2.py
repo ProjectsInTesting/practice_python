338e15faad50206e54720bde248facd85c3b4191
herrafiisdet <herrafiisdet@gmail.com>
2025-02-08 20:37:05 -0500

    update 2

---
+++ if_statement/if_practice2.py
@@ -0,0 +1,10 @@
+db_username = "admin"
+db_password = "123456"
+
+username = input ("Please enter your username:")
+password = input ("please enter your password: ")
+
+if username == db_username and password == db_password:
+    print ("You logged in Successfully!")
+else:
+    print ("wrong credentials used! please try agian")
