618fdbee4c681dc45ed7084aff88db2efcbbd5c8
herrafiisdet <herrafiisdet@gmail.com>
2025-02-08 20:18:43 -0500

    conflict 2

--- if_statement/if_practice_1.py
+++ if_statement/if_practice_1.py
@@ -5,6 +5,6 @@ username = input ("Please enter your username:")
 password = input ("please enter your password: ")
 
 if username == db_username and password == db_password:
-    print ("You have logged in Successfully!")
+    print ("You logged in Successfully!")
 else:
     print ("wrong credentials used! please try agian")
